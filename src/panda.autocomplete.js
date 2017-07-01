77cc34e0dff8282db35abda8ecabd26486b8e825
Son Bui <sonbui.gfd@gmail.com>
2017-07-01 17:19:24 +0700

    initial plugin tructure

---
+++ src/panda.autocomplete.js
@@ -0,0 +1,41 @@
+;(function($, window, undefined) {
+  'use strict';
+  var pluginName = 'autocomplete';
+
+  function Plugin(element, options) {
+    this.element = $(element);
+    this.options = $.extend({}, $.fn[pluginName].defaults, this.element.data(), options);
+    this.init();
+  }
+
+  Plugin.prototype = {
+    init: function() {
+      var that = this;
+    },
+    destroy: function() {
+      $.removeData(this.element[0], pluginName);
+    }
+  };
+
+  $.fn[pluginName] = function(options, params) {
+    return this.each(function() {
+      var instance = $.data(this, pluginName);
+      if (!instance) {
+        $.data(this, pluginName, new Plugin(this, options));
+      } else if (instance[options]) {
+        instance[options](params);
+      }
+    });
+  };
+
+  $.fn[pluginName].defaults = {
+    event: 'keyup',
+    delay: 500,
+    view: 'auto'
+  };
+
+  $(function() {
+    $('[data-' + pluginName + ']')[pluginName]();
+  });
+
+}(jQuery, window));
